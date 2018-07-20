49ec09105e9653428e14483d26f61f148ca86a01
master <wangjc@gmf-inc.com>
2018-07-20 17:11:00 +0800

    hotMv

--- src/components/index/HotMv.vue
+++ src/components/index/HotMv.vue
@@ -1,23 +1,34 @@
 <template>
-    <div class="hotmv">
-                <h3 class="clearfix">热门MV</h3>
-                <div class="swiper-container">
-                    <div class="swiper-wrapper">
-                            <div class="swiper-slide">
-                                <a href="javascript:void(0);" onclick="commPlayMv('47142682')">
-                                    <img data-src="http://img1.kwcdn.kuwo.cn/star/upload/12/12/1531907219132_.jpg"/>
-                                    <p class="mvtitle">张靓颖《不念》-空灵大气嗓音道尽古剑江湖故事</p>
-                                </a>
-                            </div>
-                            <div class="swiper-slide">
-                                <a href="javascript:void(0);" onclick="commPlayMv('46846286')">
-                                    <img data-src="http://img4.kwcdn.kuwo.cn/star/upload/11/11/1531907079227_.jpg"/>
-                                    <p class="mvtitle">霍尊携手方文山共同打造国风加成《风语咒》</p>
-                                </a>
-                            </div>
-                        
-                    </div>
-                </div>
-            </div>
+  <div class="hotmv">
+    <h3 class="clearfix">热门MV</h3>
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="(mv,index) in mvList" :key="index">
+          <a href="javascript:void(0);">
+            <img :src="mv.imgSrc" />
+            <p class="mvtitle">张靓颖《不念》-空灵大气嗓音道尽古剑江湖故事</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 
 </template>
+
+<script>
+  export default {
+    name: "HotMv",
+    data() {
+      return {
+        mvList: [{
+          mvtitle: '张靓颖《不念》-空灵大气嗓音道尽古剑江湖故事',
+          imgSrc: 'http://img1.kwcdn.kuwo.cn/star/upload/12/12/1531907219132_.jpg'
+        }, {
+          mvtitle: '霍尊携手方文山共同打造国风加成《风语咒》',
+          imgSrc: 'http://img4.kwcdn.kuwo.cn/star/upload/11/11/1531907079227_.jpg'
+        }]
+      }
+    }
+  }
+
+</script>
